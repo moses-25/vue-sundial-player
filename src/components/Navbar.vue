d9429f6d99bb84b3c4d2102a4e7ecea9c5783f5e
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-07-01 17:52:09 +0300

    Navbar component created

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<script setup>
+import "../styles/components/navbar.css";
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+</script>
+
+<template>
+    <header class="Navbar">
+        <div class="brand">
+            <span class="brand-mark">*</span>
+            <h1 class="brand-name">Sundial</h1>
+        </div>
+        <nav class="nav-links">
+            <RouterLink to="/" :class="{active: route.path == '/' }">
+                Library
+            </RouterLink>
+            <RouterLink to="/artists" :class="{active: route.path == '/artists' }">
+                Artists
+            </RouterLink>
+        </nav>
+    </header>
+
+</template>
