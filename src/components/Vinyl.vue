c2617c9a498852065ee0db8e23dc5197ae7b1de1
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-07-01 18:14:39 +0300

    Vinyl component created

---
+++ src/components/Vinyl.vue
@@ -0,0 +1,29 @@
+<script setup>
+import "../styles/components/Vinyl.css";
+defineProps({
+    color: {
+        type: String,
+        default: "#C97B4A"},  
+        isplaying: Boolean,
+        size: {
+            type: Number,
+            default: 64
+        }  
+})
+</script>
+
+<template>
+     <div class="vinyl-ring" :style="{ width: size + 'px', height: size + 'px' }">
+        <div
+            class="vinyl-disc"
+            :class="{ spinning: isPlaying }"
+            :style="{ 
+                background: `conic-gradient(
+                from 0deg, #1A1715, ${color}22, #1A1715, ${color}22, #1A1715)`
+            }"
+        >
+            <div class="vinyl-label" :style="{ background: color }"></div>
+            <div class="vinyl-grooves"></div>
+        </div>
+    </div>
+</template>
